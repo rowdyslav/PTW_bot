b84f99ef4626f909e82b7d2f8e70398ced2c728f
rowdyslav <mrdoxy960@gmail.com>
2023-06-02 21:50:38 +0300

    ss

--- cogs/Madd.py
+++ cogs/Madd.py
@@ -12,14 +12,14 @@ class Madd(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(administrator=True)
-    async def madd(self, ctx, *, form: str, count: int, voice,
+    async def madd(self, ctx, *, form: str, count: int, typ,
                    categ='Default Category Name'):
         if categ == 'Default Category Name':
             category = await ctx.guild.create_category_channel(categ)
         else:
             category = discord.utils.get(ctx.guild.categories, name=categ)
 
-        if voice:
+        if typ == '-v':
             create_channel = ctx.guild.create_voice_channel
         else:
             create_channel = ctx.guild.create_text_channel
